cbbc6064b301c4933efd0870532c1fb01a8fdeac
SnailWaffles <snailwaffles@localhost>
2024-01-02 12:54:37 +0100

    add some configuration options

--- Jellyfin.Plugin.TubeArchivistMetadata/Configuration/PluginConfiguration.cs
+++ Jellyfin.Plugin.TubeArchivistMetadata/Configuration/PluginConfiguration.cs
@@ -3,22 +3,6 @@ using MediaBrowser.Model.Plugins;
 namespace Jellyfin.Plugin.TubeArchivistMetadata.Configuration;
 
 /// <summary>
-/// The configuration options.
-/// </summary>
-public enum SomeOptions
-{
-    /// <summary>
-    /// Option one.
-    /// </summary>
-    OneOption,
-
-    /// <summary>
-    /// Second option.
-    /// </summary>
-    AnotherOption
-}
-
-/// <summary>
 /// Plugin configuration.
 /// </summary>
 public class PluginConfiguration : BasePluginConfiguration
@@ -29,29 +13,17 @@ public class PluginConfiguration : BasePluginConfiguration
     public PluginConfiguration()
     {
         // set default options here
-        Options = SomeOptions.AnotherOption;
-        TrueFalseSetting = true;
-        AnInteger = 2;
-        AString = "string";
+        URL = string.Empty;
+        Token = string.Empty;
     }
 
     /// <summary>
-    /// Gets or sets a value indicating whether some true or false setting is enabled..
+    /// Gets or sets the token setting.
     /// </summary>
-    public bool TrueFalseSetting { get; set; }
+    public string URL { get; set; }
 
     /// <summary>
-    /// Gets or sets an integer setting.
+    /// Gets or sets the token setting.
     /// </summary>
-    public int AnInteger { get; set; }
-
-    /// <summary>
-    /// Gets or sets a string setting.
-    /// </summary>
-    public string AString { get; set; }
-
-    /// <summary>
-    /// Gets or sets an enum option.
-    /// </summary>
-    public SomeOptions Options { get; set; }
+    public string Token { get; set; }
 }
--- Jellyfin.Plugin.TubeArchivistMetadata/Plugin.cs
+++ Jellyfin.Plugin.TubeArchivistMetadata/Plugin.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using Jellyfin.Plugin.TubeArchivistMetadata.Configuration;
 using MediaBrowser.Common.Configuration;
+using MediaBrowser.Common.Net;
 using MediaBrowser.Common.Plugins;
 using MediaBrowser.Model.Plugins;
 using MediaBrowser.Model.Serialization;
@@ -15,26 +18,50 @@ namespace Jellyfin.Plugin.TubeArchivistMetadata;
 public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
 {
     /// <summary>
+    /// HttpClientFactory instance.
+    /// </summary>
+    private IHttpClientFactory _httpClientFactory;
+
+    /// <summary>
     /// Initializes a new instance of the <see cref="Plugin"/> class.
     /// </summary>
     /// <param name="applicationPaths">Instance of the <see cref="IApplicationPaths"/> interface.</param>
     /// <param name="xmlSerializer">Instance of the <see cref="IXmlSerializer"/> interface.</param>
-    public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer)
+    /// <param name="httpClientFactory">Instance of the <see cref="IHttpClientFactory"/> interface.</param>
+    public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer, IHttpClientFactory httpClientFactory)
         : base(applicationPaths, xmlSerializer)
     {
         Instance = this;
+        _httpClientFactory = httpClientFactory;
     }
 
     /// <inheritdoc />
     public override string Name => "TubeArchivist";
 
     /// <inheritdoc />
-    public override Guid Id => Guid.Parse("eb5d7894-8eef-4b36-aa6f-5d124e828ce1");
+    public override Guid Id => Guid.Parse("13ddde71-8746-4f8b-8d4f-961e0a42de33");
 
     /// <summary>
     /// Gets the current plugin instance.
     /// </summary>
     public static Plugin? Instance { get; private set; }
+
+    /// <summary>
+    /// Get the HttpClient instance.
+    /// </summary>
+    /// <returns>HttpClient instance.</returns>
+    public HttpClient GetHttpClient()
+    {
+        var httpClient = _httpClientFactory.CreateClient(NamedClient.Default);
+        httpClient.DefaultRequestHeaders.UserAgent.Add(
+            new ProductInfoHeaderValue("TAMetadata", Version.ToString()));
+
+        PluginConfiguration options = this.Configuration;
+        httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", options.Token);
+        httpClient.BaseAddress = new Uri(options.URL);
+
+        return httpClient;
+    }
 
     /// <inheritdoc />
     public IEnumerable<PluginPageInfo> GetPages()
