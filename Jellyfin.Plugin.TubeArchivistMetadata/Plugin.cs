1c55233fa606edc7bfa0d68f054c7d47ef55622e
SnailWaffles <snailwaffles@localhost>
2024-01-02 09:49:58 +0100

    rename in gui

--- Jellyfin.Plugin.TubeArchivistMetadata/Plugin.cs
+++ Jellyfin.Plugin.TubeArchivistMetadata/Plugin.cs
@@ -26,7 +26,7 @@ public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
     }
 
     /// <inheritdoc />
-    public override string Name => "Template";
+    public override string Name => "TubeArchivist";
 
     /// <inheritdoc />
     public override Guid Id => Guid.Parse("eb5d7894-8eef-4b36-aa6f-5d124e828ce1");
